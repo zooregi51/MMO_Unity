73214d32f9225ab8d5f2a26e80e478c5e6339729
zooregi51 <zooregi51@gmail.com>
2021-10-13 16:57:29 +0900

    Coroutine

--- Assets/Scripts/Scenes/GameScene.cs
+++ Assets/Scripts/Scenes/GameScene.cs
@@ -3,7 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameScene : BaseScene
-{  
+{
+    // 1. 함수의 상태를 저장/복원 가능!
+        // -> 엄청 오래 걸리는 작업을 잠시 끊거나
+        // -> 원하는 타이밍에 함수를 잠시 Stop/복원하는 경우
+    // 2. return -> 우리가 원하는 타입으로 가능 (class도 가능)
+
+    Coroutine co;
+
     protected override void Init()
     {
         base.Init();
@@ -12,8 +19,28 @@ public class GameScene : BaseScene
 
         Managers.UI.ShowSceneUI<UI_Inven>();
 
-        for (int i = 0; i < 5; i++)
-            Managers.Resources.Instantiate("UnityChan");
+        co = StartCoroutine("CoExplpodeAfterSeconds", 4.0f);
+        StartCoroutine("CoStopExplode", 2.0f);
+    }
+
+    IEnumerator CoStopExplode(float seconds)
+    {
+        Debug.Log("Stop Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Stop Execute!!!!");
+        if (co != null)
+        {
+            StopCoroutine(co);
+            co = null;
+        }
+    }
+
+    IEnumerator CoExplpodeAfterSeconds(float seconds)
+    {
+        Debug.Log("Expolde Enter");
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Expolde Execute!!!!");
+        co = null;
     }
 
     public override void Clear()
